798368b2c474deae7a95f4f279bfbca30e220625
Seshan Ravikumar <seshan10@me.com>
2019-07-04 12:17:13 -0400

    Readme

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# sathya
-SathyaServer is a full framework for creating backends.
+# SathyaServer
+SathyaServer is a server framework. It is comprised of modules, such as a web server, file server, group policy, etc.
+
+**SathyaServer is still under development. Expect breaking changes!** 
+
+## Running (Linux/macOS)
+> Make sure you have NodeJS/NPM installed. This is only a requirement to run the installDepends script. SathyaServer itself will use the bundled Graal runtime.
+
+After cloning the repo install all the dependencies:
+```bash
+./installDepends.sh
+```
+This will download GraalVM, install all the node modules, build things, etc.
+
+Then, to start SathyaServer on Linux:
+```bash
+./bootstrap-linux
+```
+and on macOS:
+```bash
+./bootstrap-darwin
+```
+
+## Developing Modules
+See "Developing_Modules.md" in the docs folder.
--- installDepends.sh
+++ installDepends.sh
@@ -2,6 +2,11 @@
 echo "Installing dependencies for Sathya..."
 echo "This script requires access to the internet."
 
+if ! [ -x "$(command -v node)" ]; then
+  echo 'Error: node is not installed.' >&2
+  exit 1
+fi
+
 # Download the Graal Runtime
 echo "Downloading the GraalVM Runtime..."
 mkdir Runtime
@@ -11,16 +16,20 @@ if [[ "$OSTYPE" == "linux-gnu" ]]; then
         echo "Detected Platform Linux"
         wget https://github.com/oracle/graal/releases/download/vm-19.1.0/graalvm-ce-linux-amd64-19.1.0.tar.gz
         tar -xvf graalvm-ce-linux-amd64-19.1.0.tar.gz
+        rm graalvm-ce-linux-amd64-19.1.0.tar.gz
+        rm -rf linux
         mv graalvm-ce-19.1.0 linux
         cd ../
-        export PATH="$PWD/Runtime/linux/bin:$PATH" # Use the Graal Node and NPM
+        #export PATH="$PWD/Runtime/linux/bin:$PATH" # Use the Graal Node and NPM
 elif [[ "$OSTYPE" == "darwin"* ]]; then
         echo "Detected Platform macOS"
         wget https://github.com/oracle/graal/releases/download/vm-19.1.0/graalvm-ce-darwin-amd64-19.1.0.tar.gz
         tar -xvf graalvm-ce-darwin-amd64-19.1.0.tar.gz
+        rm graalvm-ce-darwin-amd64-19.1.0.tar.gz
+        rm -rf darwin
         mv graalvm-ce-19.1.0 darwin
         cd ../
-        export PATH="$PWD/Runtime/darwin/Contents/Home/bin:$PATH" # Use the Graal Node and NPM
+        #export PATH="$PWD/Runtime/darwin/Contents/Home/bin:$PATH" # Use the Graal Node and NPM
 else
         echo "Unknown/Unsupported OS Type."
         exit 1
@@ -38,7 +47,7 @@ echo "Installing WebUI React dependencies..."
 cd sathya-ui
 npm install
 echo "Building the ReactUI..."
-npm build
+npm run-script build
 
 # Back to root
 cd ../../../
